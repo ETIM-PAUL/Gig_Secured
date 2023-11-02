37550efc0bb39f9f78f300d86c4b2a2d922ae459
detty-dev <adetolakemi97@gmail.com>
2023-11-02 19:42:22 +0100

    tola-new-branch

--- Backend/src/Library/EscrowLibrary.sol
+++ Backend/src/Library/EscrowLibrary.sol
@@ -6,19 +6,29 @@ library EscrowCalculations {
     // when there is no conflict
 
     // function to calculate non-audit fees, calculate 7% of the total price for the gig for the platorm
-    function NonAuditFees() internal {}
+    function NonAuditFees(uint256 totalAmount) internal pure returns (uint256) {
+        return (totalAmount * 7) / 100;
+    }
 
     //function to calculate 10 percent that will be returned to Client if no audit is involved
-    function ClientNoAudit() internal {}
+    function ClientNoAudit(uint256 totalAmount) internal pure returns (uint256) {
+        return (totalAmount * 10) / 100;
+    }
 
     //function to calculate 95% of the total price and pay it to the frrelancer
-    function FreeLancerNoAudit() internal {}
+    function FreeLancerNoAudit(uint256 totalAmount) internal pure returns (uint256) {
+        return (totalAmount * 95) / 100;
+    }
 
     /// Functions to calculate
     // when audit is involved)
     // function to calculate audit fees, calculate 10% of the total price for the gig
-    function AuditFees() internal {}
+    function AuditFees(uint256 totalAmount) internal pure returns (uint256) {
+        return (totalAmount * 10) / 100;
+    }
 
     //function to calculate 92% of the total price and pay it to the freelancer when auditing is done
-    function FreeLancerAudit() internal {}
+    function FreeLancerAudit(uint256 totalAmount) internal pure returns (uint256) {
+        return (totalAmount * 92) / 100;
+    }
 }
