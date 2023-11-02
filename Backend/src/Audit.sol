0e6cd37731d142d89918d7fec41c83b20a089928
dayo2013 <emmanuelagbaje2013@gmail.com>
2023-11-02 21:58:24 +0100

    Git-secured

--- Backend/src/Audit.sol
+++ Backend/src/Audit.sol
@@ -8,24 +8,32 @@ contract Audit {
         address _auditor;
         uint currentGigs;
     }
+    mapping(address => Auditor) private auditors;
+    error InvalidAuditor();
+    error InvalidCategory();
+    error InvalidEmail();
+    error InvalidGig();
+    error InvalidGigType();
+    error InvalidGigStatus();
+    
 
     address owner;
 
     Auditor[] private Auditors;
-    event AuditorRemoved(address indexed removedAuditor);
+    
 
     function becomeAuditor() public {}
 
     function confirmAuditor() public {}
 
-    function removeAuditor(address _auditor) public onlyowner{
-        require(_auditor!= address(0));//auditor most not be 0x0
-        require(_auditor != owner, "Owner cannot be removed as an auditor");
-        emit AuditorRemoved(auditorToRemove);
+    function removeAuditor(uint _Audit) public {
+        delete Auditors[_Audit];
+        
+        
 
     }
 
-    function getAuditors() public view returns(Auditor[] memory){
+    function assignAuditor() public view returns(Auditor[] memory){
         return Auditors;
 
     }
