3626cf632cc015473e508b56b16289fd38c496f3
MACBOOK <natachigram@gmail.com>
2023-11-04 17:33:53 +0100

    slight changes

---
+++ Backend/test/Audit.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Audit} from "../src/Audit.sol";
+
+contract CounterTest is Test {
+    Audit public audit;
+    address _governance = address(0x124);
+
+    function setUp() public {
+        audit = new Audit(_governance);
+    }
+
+    function test_Increment() public {}
+}
