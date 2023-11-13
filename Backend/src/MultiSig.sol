c9f31c753f6c3c7f75f5d8136aaf2829ef9d1747
dayo2013 <emmanuelagbaje2013@gmail.com>
2023-11-13 11:59:24 +0100

    dy

--- Backend/src/MultiSig.sol
+++ Backend/src/MultiSig.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
+/// @title MultiSig - Multi-signature wallet contract
+/// @notice This contract implements a multi-signature wallet, allowing multiple addresses to approve transactions.
+/// @dev All transactions are represented by the Transaction struct.
 
 contract MultiSig {
     struct Transaction {
@@ -9,25 +12,47 @@ contract MultiSig {
         uint numberOfApproval;
         bool isActive;
     }
-    address[] Admins;
-    uint constant MINIMUM = 3;
-    uint transactionId;
+    /// @notice List of administrator addresses.
+    address[] public Admins;
 
-    mapping(address => bool) isAdmin;
-    mapping(uint => Transaction) transaction;
-    mapping(uint => mapping(address => bool)) hasApproved;
+    /// @dev Minimum number of approvals required for a transaction to be executed.
+    uint constant public MINIMUM = 3;
 
+    /// @dev Transaction ID counter.
+    uint public transactionId;
+
+
+    /// @notice Mapping to track whether an address is an administrator.
+    mapping(address => bool) public isAdmin;
+
+    /// @notice Mapping to store transaction details based on transaction ID.
+    mapping(uint => Transaction) public transaction;
+
+    /// @notice Mapping to track approval status for each transaction by each administrator.
+    mapping(uint => mapping(address => bool)) public hasApproved;
+
+/// @param position The position in the address array where the error occurred.
     error InvalidAddress(uint position);
+    /// @param number The invalid number of administrators.
     error InvalidAdminNumber(uint number);
+    /// @param _addr The duplicate address being added.
     error duplicate(address _addr);
+    /// @dev Event emitted when a new transaction is created.
+    /// @param who The address triggering the creation of the transaction.
+    /// @param spender The address to which funds will be sent.
+    /// @param amount The amount of funds to be sent in the transaction.
 
     event Create(address who, address spender, uint amount);
 
+    /// @dev Modifier to restrict access to administrators.
     modifier onlyAdmin() {
         require(isAdmin[msg.sender], "Not a Valid Admin");
         _;
     }
-
+    /// @dev Constructor to initialize the contract with a list of administrators.
+    /// @param _admins An array of administrator addresses.
+    /// @dev Requires at least MINIMUM administrators and ensures unique and non-zero addresses.
+    
     constructor(address[] memory _admins) payable {
         if (_admins.length < MINIMUM) {
             revert InvalidAdminNumber(MINIMUM);
@@ -44,6 +69,10 @@ contract MultiSig {
         }
         Admins = _admins;
     }
+    /// @dev Function to create a new transaction.
+    /// @param amount The amount of funds to be sent in the transaction.
+    /// @param _spender The address to which funds will be sent.
+    /// @dev Increments the transactionId, initializes the transaction details, emits a Create event, and calls AprroveTransaction.
 
     function createTransaction(
         uint amount,
@@ -57,6 +86,7 @@ contract MultiSig {
         emit Create(msg.sender, _spender, amount);
         AprroveTransaction(transactionId);
     }
+    
 
     function AprroveTransaction(uint id) public onlyAdmin {
         //check if addmin has not approved yet;
@@ -74,18 +104,24 @@ contract MultiSig {
             sendtransaction(id);
         }
     }
+    /// @dev Function to approve a transaction.
+    /// @param id The ID of the transaction to be approved.
+    /// @dev Checks if the administrator has not approved yet, updates approval status, and processes the transaction if approval conditions are met.
 
     function sendtransaction(uint id) internal {
         Transaction storage _transaction = transaction[id];
         payable(_transaction.spender).transfer(_transaction.amount);
         _transaction.isActive = false;
     }
+    
 
     function calculateMinimumApproval() public view returns (uint MinApp) {
         uint size = Admins.length;
         MinApp = (size * 70) / 100;
     }
-
+ /// @dev Function to calculate the minimum number of approvals.
+    /// @return MinApp The calculated minimum number of approvals.
+    /// @dev The minimum number of approvals is set to 70% of the total number of administrators.
     function getTransaction(
         uint id
     ) external view returns (Transaction memory) {
