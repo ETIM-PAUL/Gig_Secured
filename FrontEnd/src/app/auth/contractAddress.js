e9b8f17fcd6cf6ad2131bffde50955bbedd2572d
etim <etimpaul22@gmail.com>
2023-11-20 20:06:26 +0100

    corrected changes

--- FrontEnd/src/app/auth/contractAddress.js
+++ FrontEnd/src/app/auth/contractAddress.js
@@ -1,4 +1,4 @@
-export const factoryAddress = "0x0F6479feeF6864aE529B5c12aAe3c7CE38f6daCb";
+export const factoryAddress = "0x64B706A7Eaa2Ab40fA4ABC7C4d604Be04FCc42Eb";
 export const auditorAddress = "0x33eb31eb691Dfe418E5a444C6194fB9818195385"
 export const usdc = "0x15c03c8276F6B1843DA9d4d3681591B4EeFc7a51"
 export const vrfAddress = "0xa728392F4369A5d1E3386ea45EFD1c39e4fC5567"
